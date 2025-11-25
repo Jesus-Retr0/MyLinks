6911a0db57dc04e85dd5f6a6a98c2f49044b4036
Jesus-Retr0 <delapazjesus910@yahoo.com>
2025-11-25 15:02:24 -0400

    Added Email

--- script.js
+++ script.js
@@ -6,6 +6,7 @@ const data = {
     },
     links: [
         { url: "https://jesus-retr0.github.io/Portfolio", name: "Portfolio", iconUrl: "./assets/file-earmark-fill.svg" },
+        { url: "mailto:jesus_codes1@proton.me", name: "Email", iconUrl: "./assets/envelope.svg" },
         { url: "https://github.com/Jesus-Retr0", name: "GitHub", iconUrl: "https://github.githubassets.com/favicons/favicon-dark.png" }
     ],
     // suggestedProjects is an array of objects (JSON-style). Edit this data to add/remove projects.
